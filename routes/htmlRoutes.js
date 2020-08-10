ca88f7ba169d1f3a11c5276b44ee60dc687aa915
Sammy <sammy.cullinan@gmail.com>
2020-08-09 17:38:28 -0700

    test

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,7 +4,7 @@ const router = require('express').Router();
 router.get('/notes', function(req, res) {
     res.sendFile(path.join(__dirname, '../Develop/public/notes.html'));
 });
-router.get('*', function(req, res) {
+router.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../Develop/public/index.html'));
 });        
 
